fc16d7cacdd81ab1a68fca0914d4a68429dc7f95
Marra Cloud <34797030+apoloravi@users.noreply.github.com>
2025-12-23 14:34:31 -0300

    Add configuration file for NeedZap settings

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * CONFIGURAÇÃO DO NEEDZAP
+ * ----------------------
+ * Preencha corretamente os dados abaixo
+ */
+
+define('NEEDZAP_API_URL', 'https://api.needzap.net/v1/api/external');
+
+// Token JWT fornecido pelo Whazing / NeedZap
+define('NEEDZAP_TOKEN', 'COLE_SEU_TOKEN_AQUI');
+
+// External Key do painel NeedZap
+define('NEEDZAP_EXTERNAL_KEY', 'COLE_SUA_EXTERNAL_KEY_AQUI');
